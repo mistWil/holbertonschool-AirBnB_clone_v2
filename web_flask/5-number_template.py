073396934c8b24dcf7992fb5c9cd292cb180fc96
mistWil <7448@holbertonstudents.com>
2024-04-10 11:49:08 +0200

    add modifications

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,49 +1,51 @@
 #!/usr/bin/python3
 
 
-""" Create server with default main page and number template """
+"""
+Script that starts a Flask web application
+"""
 
 
-from flask import Flask
-from markupsafe import escape
-from flask import render_template
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Displays 'HBNB'"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    text = text.replace('_', ' ')
-    return "C " + escape(text)
+def c_text(text="is cool"):
+    """Displays 'C ' followed by the value of the text variable"""
+    return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_txt(text):
-    text = text.replace('_', ' ')
-    return "Python " + escape(text)
+def python_text(text="is cool"):
+    """Displays 'Python ' followed by the value of the text variable"""
+    return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def display_number(n):
+def number(n):
+    """Displays 'n is a number' only if n is an integer"""
     return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', n=n)
+    """Displays an HTML page only if n is an integer"""
+    return render_template('number.html', n=n)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)

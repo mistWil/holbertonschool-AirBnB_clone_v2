667db5d042e62c73a1c2681d6c26936d591982ad
mistWil <7448@holbertonstudents.com>
2024-04-10 11:11:11 +0200

    task10

---
+++ web_flask/9-states.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+
+"""
+Script that starts a Flask web application
+"""
+
+
+from flask import Flask, render_template, abort
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """
+    Removes the current SQLAlchemy Session after each request
+    """
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states_and_cities(id=None):
+    """
+    Displays an HTML page with a list of all State objects
+    or a list of cities for a given state
+    """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda x: x.name)
+    if id:
+        state = storage.get(State, id)
+        if state:
+            cities = sorted(state.cities, key=lambda x: x.name)
+            return render_template('9-states.html',
+                                   states=states, state=state, cities=cities)
+        else:
+            abort(404)
+    else:
+        return render_template('9-states.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
